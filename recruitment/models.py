20fba8633e37696ba3483a8aac2a9e1034737840
Chaitanya <princechaitu21@gmail.com>
2021-10-19 00:08:07 +0530

    Create User model.
    Co-authored-by: Dinesh <chdinesh1089@gmail.com>

--- recruitment/models.py
+++ recruitment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+
+    from_tnp = models.BooleanField(default=False)  # Everyone who's not from TnP is a student. Only they'll have a StudentProfile.
+    mobile_number = models.CharField(max_length=10, blank=True)
+    
+
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    roll_number = models.CharField(max_length=10, unique=True)
+    registration_number = models.CharField(max_length=10, unique=True)
