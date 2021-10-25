097851b5a252f2e9bc0d1b107797f5a5a15b1409
srinija2002 <68015061+srinija2002@users.noreply.github.com>
2021-10-25 16:28:36 +0530

    editprofile js modified

--- recruitment/static/recruitment/js/editProfile.js
+++ recruitment/static/recruitment/js/editProfile.js
@@ -10,7 +10,13 @@ function backlogs(){
     var check = document.getElementById("input-backlogs");
     if(check.value == "OutstandingBacklogs"){
         document.getElementById("outBacklogs").style.display="flex";
-    }else{
+        document.getElementById("backlogs-cleared").style.display="none";
+    }else if(check.value == "NoOutstandingBacklogs"){
+        document.getElementById("backlogs-cleared").style.display="flex";
         document.getElementById("outBacklogs").style.display="none";
+    }
+    else{
+        document.getElementById("outBacklogs").style.display="none";
+        document.getElementById("backlogs-cleared").style.display="none";
     }
 }
