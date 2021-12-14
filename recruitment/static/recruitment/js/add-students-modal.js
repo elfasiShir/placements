e09e2fce11203e18288c3b2f4e0af9a423bed7ce
Chaitanya <princechaitu21@gmail.com>
2021-12-14 08:27:15 +0530

    fetch data from django and update add-students model

--- recruitment/static/recruitment/js/add-students-modal.js
+++ recruitment/static/recruitment/js/add-students-modal.js
@@ -21,13 +21,22 @@ function selectedFilter(element) {
         }
     });
 }
+const addStudentRow=(student_id,roll,name,parentDivId)=>{
+    document.querySelector(parentDivId).insertAdjacentHTML(
+        "afterbegin",
+        `<li user_id="${student_id}"><div class="flex-row student-row"> <h3 class="student-roll-number">${roll} </h3> <h3 class="student-name">${name}</h3>  <div class="student-row-button remove-add-button"onclick="addStudent(this)">&#43;</div></div></li>
+`
+    );
+
+}
 
 function addStudent(element) {
     let row = element.parentNode.parentNode;
     let roll = row.getElementsByClassName('student-roll-number')[0].innerText;
     let name = row.getElementsByClassName('student-name')[0].innerText;
     let student_id = row.getAttribute("user_id");
-    document.querySelector("#added-student-list").insertAdjacentHTML(
+    let parentDiv="#added-student-list"
+    document.querySelector(parentDiv).insertAdjacentHTML(
         "afterbegin",
         `<li user_id="${student_id}"><div class="flex-row student-row"> <h3 class="student-roll-number">${roll} </h3> <h3 class="student-name">${name}</h3>  <div class="student-row-button remove-add-button"onclick="removeStudent(this)">&#10005;</div></div></li>
 `
@@ -45,14 +54,40 @@ function removeStudent(element) {
     let roll = row.getElementsByClassName('student-roll-number')[0].innerText;
     let name = row.getElementsByClassName('student-name')[0].innerText;
     let student_id = row.getAttribute("user_id");
-    document.querySelector("#student-list").insertAdjacentHTML(
-        "afterbegin",
-        `<li user_id="${student_id}"><div class="flex-row student-row"> <h3 class="student-roll-number">${roll} </h3> <h3 class="student-name">${name}</h3>  <div class="student-row-button remove-add-button"onclick="addStudent(this)">&#43;</div></div></li>
-`
-    );
+    let listToAdd="#student-list"
+    addStudentRow(student_id,roll,name,listToAdd)
+
     row.remove();
 
     let student_list = document.querySelector("#selected-student-list-input");
     let student_input = student_list.querySelector(`input[value="${student_id}"]`);
     student_input.remove();
+}
+var didFetch=false
+
+async function fetchStudents(){
+    if(! window.didFetch)
+    {
+    let round=document.getElementsByName("round_number")[0].value -1 
+    let jobId=document.getElementsByName("job")[0].value 
+    console.log(round,jobId)
+    let host = window.location.protocol + "//" + window.location.host + "/";
+    let url=host+`job-${jobId}/round-${round}/get_students/`
+    try{
+    let response = await fetch(url);
+    let students = await response.json();
+
+    students.forEach(student => {
+        let {id,first_name,last_name}=student
+        let roll=12
+        let name=first_name+" "+last_name
+        addStudentRow(id,roll,name,"#student-list")        
+    });
+    window.didFetch=true
+    }
+    catch(e){
+        alert(e)
+    }
+    
+}
 }
